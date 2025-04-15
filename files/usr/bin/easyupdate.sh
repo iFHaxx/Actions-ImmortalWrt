4e80617ad8d75f25671fb80e0ccc78c595fc722d
iFHaxx <anzclan@proton.me>
2025-04-16 07:47:15 +0800

    Update easyupdate.sh

--- files/usr/bin/easyupdate.sh
+++ files/usr/bin/easyupdate.sh
@@ -1,179 +1,175 @@
 #!/bin/bash
-# https://github.com/uJZk/Actions-ImmortalWrt
-# Easy Update Script by uJZk
-# Modified from [luci-app-easyupdate](https://github.com/sundaqiang/openwrt-packages/tree/master/luci-app-easyupdate)
 
 function checkEnv() {
-	if !type sysupgrade >/dev/null 2>&1; then
-		writeLog 'Your firmware does not contain sysupgrade and does not support automatic updates(您的固件未包含sysupgrade,暂不支持自动更新)'
-		exit
-	fi
+    if ! type sysupgrade >/dev/null 2>&1; then
+        writeLog 'Your firmware does not contain sysupgrade and does not support automatic updates'
+        exit
+    fi
 }
 
 function writeLog() {
-	now_time='['$(date +"%Y-%m-%d %H:%M:%S")']'
-	echo ${now_time} $1 | tee -a '/tmp/easyupdatemain.log'
+    now_time='['$(date +"%Y-%m-%d %H:%M:%S")']'
+    echo ${now_time} $1 | tee -a '/tmp/easyupdatemain.log'
 }
 
 function shellHelp() {
-	checkEnv
-	cat <<EOF
-Easy Update Script by uJZk
-Your firmware already includes Sysupgrade and supports automatic updates(您的固件已包含sysupgrade,支持自动更新)
-参数:
-    -c                     Get the cloud firmware version(获取云端固件版本)
-    -d                     Download cloud Firmware(下载云端固件)
-    -f filename                Flash firmware(刷写固件)
-    -u                     One-click firmware update(一键更新固件)
+    checkEnv
+    cat <<EOF
+Your firmware already includes Sysupgrade and supports automatic updates
+Parameters:
+    -c                     Get the cloud firmware version
+    -d                     Download cloud Firmware
+    -f filename            Flash firmware
+    -u                     One-click firmware update
 EOF
 }
 
 function getCloudVer() {
-	checkEnv
-	github=$(cat /etc/openwrt_release | sed -n "s/DISTRIB_GITHUB='\(\S*\)'/\1/p")
-	github=(${github//// })
-	curl "https://api.github.com/repos/${github[2]}/${github[3]}/releases/latest" | jsonfilter -e '@.tag_name' | sed -e 's/.*\([0-9]\{12\}.*\)/\1/'
+    checkEnv
+    github=$(cat /etc/openwrt_release | sed -n "s/DISTRIB_GITHUB='\(\S*\)'/\1/p")
+    github=(${github//// })
+    curl "https://api.github.com/repos/${github[2]}/${github[3]}/releases/latest" | jsonfilter -e '@.tag_name' | sed -e 's/.*\([0-9]\{12\}.*\)/\1/'
 }
 
 function downCloudVer() {
-	checkEnv
-	writeLog 'Get github project address(读取github项目地址)'
-	github=$(cat /etc/openwrt_release | sed -n "s/DISTRIB_GITHUB='\(\S*\)'/\1/p")
-	writeLog "Github project address(github项目地址):$github"
-	github=(${github//// })
-	writeLog 'Check whether EFI firmware is available(判断是否EFI固件)'
-	if [ -d "/sys/firmware/efi/" ]; then
-		suffix="combined-efi.img.gz"
-	else
-		suffix="combined.img.gz"
-	fi
-	writeLog "Whether EFI firmware is available(是否EFI固件):$suffix"
-	writeLog 'Get the cloud firmware link(获取云端固件链接)'
-	url=$(curl "https://api.github.com/repos/${github[2]}/${github[3]}/releases/latest" | jsonfilter -e '@.assets[*].browser_download_url' | sed -n "/$suffix/p")
-	writeLog "Cloud firmware link(云端固件链接):$url"
-	mirror=''
-	writeLog "Use mirror URL(使用镜像网站):$mirror"
-	fileName=(${url//// })
-	curl -o "/tmp/${fileName[7]}-sha256" -L "$mirror${url/${fileName[7]}/sha256sums}"
-	curl -m 10000 -o "/tmp/${fileName[7]}" -L "$mirror$url" >/tmp/easyupdate.log 2>&1 &
-	writeLog 'Start downloading firmware, log output in /tmp/easyupdate.log(开始下载固件，日志输出在/tmp/easyupdate.log)'
+    checkEnv
+    writeLog 'Get github project address'
+    github=$(cat /etc/openwrt_release | sed -n "s/DISTRIB_GITHUB='\(\S*\)'/\1/p")
+    writeLog "Github project address: $github"
+    github=(${github//// })
+    writeLog 'Check whether EFI firmware is available'
+    if [ -d "/sys/firmware/efi/" ]; then
+        suffix="combined-efi.img.gz"
+    else
+        suffix="combined.img.gz"
+    fi
+    writeLog "Whether EFI firmware is available: $suffix"
+    writeLog 'Get the cloud firmware link'
+    url=$(curl "https://api.github.com/repos/${github[2]}/${github[3]}/releases/latest" | jsonfilter -e '@.assets[*].browser_download_url' | sed -n "/$suffix/p")
+    writeLog "Cloud firmware link: $url"
+    mirror=''
+    writeLog "Use mirror URL: $mirror"
+    fileName=(${url//// })
+    curl -o "/tmp/${fileName[7]}-sha256" -L "$mirror${url/${fileName[7]}/sha256sums}"
+    curl -m 10000 -o "/tmp/${fileName[7]}" -L "$mirror$url" >/tmp/easyupdate.log 2>&1 &
+    writeLog 'Start downloading firmware, log output in /tmp/easyupdate.log'
 }
 
 function flashFirmware() {
-	checkEnv
-	if [[ -z "$file" ]]; then
-		writeLog 'Please specify the file name(请指定文件名)'
-	else
-		writeLog 'Get whether to save the configuration(读取是否保存配置)'
-		writeLog "Whether to save the configuration(读取是否保存配置):$res"
-		writeLog 'Start flash firmware, log output in /tmp/easyupdate.log(开始刷写固件，日志输出在/tmp/easyupdate.log)'
-		sysupgrade /tmp/$file >/tmp/easyupdate.log 2>&1 &
-	fi
+    checkEnv
+    if [[ -z "$file" ]]; then
+        writeLog 'Please specify the file name'
+    else
+        writeLog 'Get whether to save the configuration'
+        writeLog "Whether to save the configuration: $res"
+        writeLog 'Start flash firmware, log output in /tmp/easyupdate.log'
+        sysupgrade /tmp/$file >/tmp/easyupdate.log 2>&1 &
+    fi
 }
 
 function checkSha() {
-	if [[ -z "$file" ]]; then
-		for filename in $(ls /tmp)
-		do
-			if [[ "${filename#*.}" = "img.gz" && "${filename}" == *"-combined"* ]]; then
-				file=$filename
-			fi
-		done
-	fi
-	cd /tmp && sha256sum -c <(grep $file $file-sha256)
+    if [[ -z "$file" ]]; then
+        for filename in $(ls /tmp)
+        do
+            if [[ "${filename#*.}" = "img.gz" && "${filename}" == *"-combined"* ]]; then
+                file=$filename
+            fi
+        done
+    fi
+    cd /tmp && sha256sum -c <(grep $file $file-sha256)
 }
 
 function updateCloud() {
-	checkEnv
-	writeLog 'Get the local firmware version(获取本地固件版本)'
-	lFirVer=$(cat /etc/openwrt_release | sed -n "s/DISTRIB_VERSIONS='.*\([0-9]\{12\}\).*'/\1/p")
-	writeLog "Local firmware version(本地固件版本):$lFirVer"
-	writeLog 'Get the cloud firmware version(获取云端固件版本)'
-	cFirVer=$(getCloudVer)
-	writeLog "Cloud firmware version(云端固件版本):$cFirVer"
-	lFirVer=$(date -d "${lFirVer:0:4}-${lFirVer:4:2}-${lFirVer:6:2} ${lFirVer:8:2}:${lFirVer:10:2}" +%s)
-	cFirVer=$(date -d "${cFirVer:0:4}-${cFirVer:4:2}-${cFirVer:6:2} ${cFirVer:8:2}:${cFirVer:10:2}" +%s)
-	if [ $cFirVer -gt $lFirVer ]; then
-		writeLog 'Need to be updated(需要更新)'
-		checkShaRet=$(checkSha)
-		if [[ $checkShaRet =~ 'OK' ]]; then
-			writeLog 'Check completes(检查完成)'
-			file=${checkShaRet:0:-4}
-			flashFirmware
-		else
-			downCloudVer
-			i=0
-			while [ $i -le 100 ]; do
-				log=$(cat /tmp/easyupdate.log)
-				str='transfer closed'
-				if [[ $log =~ $str ]]; then
-					writeLog 'Download error(下载出错)'
-					i=101
-					break
-				else
-					str='Could not resolve host'
-					if [[ $log =~ $str ]]; then
-						writeLog 'Download error(下载出错)'
-						i=101
-						break
-					else
-						str='100\s.+M\s+100.+--:--:--'
-						if [[ $log =~ $str ]]; then
-							writeLog 'Download completes(下载完成)'
-							i=100
-							break
-						else
-							echo $log | sed -n '$p'
-							if [[ $i -eq 99 ]]; then
-								writeLog 'Download the timeout(下载超时)'
-								break
-							fi
-						fi
-					fi
-				fi
-				let i++
-				sleep 3
-			done
-			if [[ $i -eq 100 ]]; then
-				writeLog 'Prepare flash firmware(准备刷写固件)'
-				checkShaRet=$(checkSha)
-				if [[ $checkShaRet =~ 'OK' ]]; then
-					writeLog 'Check completes(检查完成)'
-					file=${checkShaRet:0:-4}
-					flashFirmware
-				else
-					writeLog 'Check error(检查出错)'
-				fi
-			fi
-		fi
-	else
-		writeLog "Is the latest(已是最新)"
-	fi
+    checkEnv
+    writeLog 'Get the local firmware version'
+    lFirVer=$(cat /etc/openwrt_release | sed -n "s/DISTRIB_VERSIONS='.*\([0-9]\{12\}\).*'/\1/p")
+    writeLog "Local firmware version: $lFirVer"
+    writeLog 'Get the cloud firmware version'
+    cFirVer=$(getCloudVer)
+    writeLog "Cloud firmware version: $cFirVer"
+    lFirVer=$(date -d "${lFirVer:0:4}-${lFirVer:4:2}-${lFirVer:6:2} ${lFirVer:8:2}:${lFirVer:10:2}" +%s)
+    cFirVer=$(date -d "${cFirVer:0:4}-${cFirVer:4:2}-${cFirVer:6:2} ${cFirVer:8:2}:${cFirVer:10:2}" +%s)
+    if [ $cFirVer -gt $lFirVer ]; then
+        writeLog 'Need to be updated'
+        checkShaRet=$(checkSha)
+        if [[ $checkShaRet =~ 'OK' ]]; then
+            writeLog 'Check completes'
+            file=${checkShaRet:0:-4}
+            flashFirmware
+        else
+            downCloudVer
+            i=0
+            while [ $i -le 100 ]; do
+                log=$(cat /tmp/easyupdate.log)
+                str='transfer closed'
+                if [[ $log =~ $str ]]; then
+                    writeLog 'Download error'
+                    i=101
+                    break
+                else
+                    str='Could not resolve host'
+                    if [[ $log =~ $str ]]; then
+                        writeLog 'Download error'
+                        i=101
+                        break
+                    else
+                        str='100\s.+M\s+100.+--:--:--'
+                        if [[ $log =~ $str ]]; then
+                            writeLog 'Download completes'
+                            i=100
+                            break
+                        else
+                            echo $log | sed -n '$p'
+                            if [[ $i -eq 99 ]]; then
+                                writeLog 'Download timeout'
+                                break
+                            fi
+                        fi
+                    fi
+                fi
+                let i++
+                sleep 3
+            done
+            if [[ $i -eq 100 ]]; then
+                writeLog 'Prepare flash firmware'
+                checkShaRet=$(checkSha)
+                if [[ $checkShaRet =~ 'OK' ]]; then
+                    writeLog 'Check completes'
+                    file=${checkShaRet:0:-4}
+                    flashFirmware
+                else
+                    writeLog 'Check error'
+                fi
+            fi
+        fi
+    else
+        writeLog "Is the latest"
+    fi
 }
 
 if [[ -z "$1" ]]; then
-	shellHelp
+    shellHelp
 else
-	case $1 in
-	-c)
-		getCloudVer
-		;;
-	-d)
-		downCloudVer
-		;;
-	-f)
-		file=$2
-		flashFirmware
-		;;
-	-k)
-		file=$2
-		checkSha
-		;;
-	-u)
-		updateCloud
-		;;
-	*)
-		shellHelp
-		;;
-	esac
+    case $1 in
+    -c)
+        getCloudVer
+        ;;
+    -d)
+        downCloudVer
+        ;;
+    -f)
+        file=$2
+        flashFirmware
+        ;;
+    -k)
+        file=$2
+        checkSha
+        ;;
+    -u)
+        updateCloud
+        ;;
+    *)
+        shellHelp
+        ;;
+    esac
 fi
