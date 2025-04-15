cae5dc29293e61aca42ddff1af029d9ceee9d95a
iFHaxx <anzclan@proton.me>
2025-04-16 07:48:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,17 +1,4 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
-
-# Uncomment feed sources
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+#!/bin/bash 
 
 # Remove feed sources
 sed -i '/telephony/d' feeds.conf.default
