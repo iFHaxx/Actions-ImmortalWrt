a238a4eac5dc8a80e12edeb8c5c8757e19deedb2
iFHaxx <anzclan@proton.me>
2025-04-16 15:08:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -8,4 +8,7 @@ echo 'src-git daed https://github.com/QiuSimons/luci-app-daed' >> feeds.conf.def
 echo 'src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon' >> feeds.conf.default
 echo 'src-git wrtbwmon_luci https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
 echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
+# Add xiaorouji's Passwall2 repo
+echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2" >> feeds.conf.default
+
  
