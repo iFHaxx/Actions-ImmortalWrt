ab660b74ed80dccceff0a25da3a0e1c4bf785999
iFHaxx <anzclan@proton.me>
2025-04-16 08:11:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,10 +1,17 @@
-#!/bin/bash 
+#!/bin/bash
 
-# Remove feed sources
+# Remove unwanted feed sources
 sed -i '/telephony/d' feeds.conf.default
 
-# Add feed sources
+# Add custom feed sources
 echo 'src-git daed https://github.com/QiuSimons/luci-app-daed' >> feeds.conf.default
 echo 'src-git wrtbwmon https://github.com/brvphoenix/wrtbwmon' >> feeds.conf.default
 echo 'src-git wrtbwmon_luci https://github.com/brvphoenix/luci-app-wrtbwmon' >> feeds.conf.default
 echo 'src-git sirpdboy https://github.com/sirpdboy/sirpdboy-package' >> feeds.conf.default
+
+# Add ImmortalWrt 24.10 official feeds
+echo 'src-git-full base https://github.com/immortalwrt/immortalwrt.git' >> feeds.conf.default
+echo 'src-git packages https://github.com/immortalwrt/packages.git' >> feeds.conf.default
+echo 'src-git luci https://github.com/immortalwrt/luci.git' >> feeds.conf.default
+echo 'src-git routing https://github.com/openwrt/routing.git' >> feeds.conf.default
+echo 'src-git telephony https://github.com/openwrt/telephony.git' >> feeds.conf.default
